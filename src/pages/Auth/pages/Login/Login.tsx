2b61b717ce34f9244fb9cf81ab912fd2c0b9dfb0
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-12-17 18:29:42 +0200

    Implement `Login` ui component

--- src/pages/Auth/pages/Login/Login.tsx
+++ src/pages/Auth/pages/Login/Login.tsx
@@ -1,12 +1,23 @@
-interface IProps {
-    
-}
-const Login = ({}: IProps) => {
-    return (
-        <div>
-            <p></p>
+import { Link } from "react-router-dom"
+import Input from "../../../../components/ui/Input"
+import { loginFormFeilds } from "../../../../constants"
 
-        </div>
+interface IProps {
+
+}
+const Login = ({ }: IProps) => {
+    return (
+            <div className="flex flex-col gap-4 w-full">
+                <p className="text-[18px] text-center font-semibold">Sign up your account</p>
+                <form className="flex flex-col gap-6 mt-4">
+                    {loginFormFeilds.map((feild, index) => (
+                        <Input key={index} placeholder={feild.placeholder} type={feild.type} name={feild.name} />
+                    ))}
+
+                    <button type="submit" className="bg-[--primary] mt-3 text-white hover:bg-[--primary-dark] transition">Sign me up</button>
+                </form>
+                <p>Don't have an account? <Link to='/auth/register'>Sign up</Link></p>
+            </div>
     )
 }
 
--- src/pages/Auth/pages/Register/Register.tsx
+++ src/pages/Auth/pages/Register/Register.tsx
@@ -6,12 +6,12 @@ const Register = () => {
     return (
         <div className="flex flex-col gap-4 w-full">
             <p className="text-[18px] text-center font-semibold">Sign up your account</p>
-            <form className="flex flex-col gap-4 mt-4">
+            <form className="flex flex-col gap-6 mt-4">
                 {registerFormFeilds.map((feild, index) => (
                     <Input key={index} placeholder={feild.placeholder} type={feild.type} name={feild.name} />
                 ))}
 
-                <button type="submit" className="bg-[--primary] mt-4 text-white hover:bg-[--primary-dark] transition">Sign me up</button>
+                <button type="submit" className="bg-[--primary] mt-3 text-white hover:bg-[--primary-dark] transition">Sign me up</button>
             </form>
             <p>Already have an account? <Link to='/auth/login'>Sign in</Link></p>
         </div>
